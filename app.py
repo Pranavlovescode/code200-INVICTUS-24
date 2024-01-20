655e45b3cc2a900be970b8062353bfbd054934e7
Pranavlovescode <pranavtitambe04@gmail.com>
2024-01-20 20:36:06 +0530

    Searching using pandas

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask,request
+from flask import Flask,request,jsonify
 from pymongo import MongoClient
 from flask_bcrypt import Bcrypt
-
+from datetime import datetime
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -10,9 +11,12 @@ db = client['InternData'] #Database name
 
 bcrypt = Bcrypt(app)
 
+df = pd.read_csv('./glassdoor_jobs.csv')
+
 @app.route('/')
 def helloWorld():
     return '<p>Hello World</p>'
+
 
 @app.route('/pranav')
 def pranav():
@@ -37,13 +41,74 @@ def intern():
             'password':hashPassword,
             'phoneNumber':phoneNumber,
             'dob':dob,
-            'clg':clg
+            'clg':clg,
+            'date':datetime.today()
         })
         print(result)
         return str(result)
     else:
         return "Invalid request method"
+
+
+@app.route('/carrier-form',methods=["POST"])
+def carrier():
+    if request.method == 'POST':
+        data = request.get_json()        
+        skillset = data['skillset']
+        self_rating = data['self_rating']
+        company = data['conpany']
         
+        result = db.signupData.find_one({
+            'skillset':skillset,
+            'self_rating':self_rating,
+            'company':company,
+            'date':datetime.today()
+        })
+        print(result)
+        return str(result)
+    else:
+        return "Invalid request method"
+
+@app.route('/company-info',methods=['POST'])
+def companyData():    
+    if request.method=='POST':
+        res = request.get_json()
+        cname = res['cname']
+        cpass = res['cpass']
+        cemail = res['cemail']
+        hashPass = bcrypt.generate_password_hash(cpass).decode('utf-8')
+        cadd=res['cadd']
+        cphone=res['cphone']
+        result = db.companySignUpData.insert_one({
+            'cname':cname,
+            'cemail':cemail,
+            'cpass':hashPass,
+            'cadd':cadd,
+            'cphone':cphone, 
+            'date':datetime.today()           
+        })
+        print(result)
+        return str(result)   
+    
+
+@app.route('/secure/data/pyttsx3/get-the-info')     
+def secretData():    
+    df.rename(columns={'Unnamed: 0': 'index'}, inplace=True)
+    df.set_index('index', inplace=True)
+    df.drop(columns=['Founded', 'Industry','Competitors'], axis=1, inplace=True)
+    data = df.to_dict('records')    
+    db.glassdoor_jobs.insert_many(data)
+
+
+# seaeching technique using pandas
+@app.route('/search/your-job')
+def search():
+    df.head(50)
+    job_title = input("Enter job title keyword: ")
+    filtered_df = df[df['Job Title'].str.contains(job_title, case=False, na=False)]
+    print(filtered_df)
+    return(filtered_df)
+    
 
 
 if __name__=='__main__':
